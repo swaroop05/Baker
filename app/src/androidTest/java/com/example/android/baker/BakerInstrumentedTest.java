bf0ca829f618beb9011568e593d7ecae6075ccaf
Swaroop Nagarajan <meetswaroop05@gmail.com>
2018-12-03 22:54:22 -0500

    gradle dependencies update to accommodate running espresso test

--- app/src/androidTest/java/com/example/android/baker/BakerInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/baker/BakerInstrumentedTest.java
@@ -3,7 +3,9 @@ package com.example.android.baker;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,7 +17,9 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class BakerInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -23,4 +27,13 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.android.baker", appContext.getPackageName());
     }
+
+    @Test
+    public void bakerRecipesListValidation() throws Exception {
+
+
+
+    }
+
+
 }
